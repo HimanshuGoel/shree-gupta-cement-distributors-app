07b904b7aa7922edc2bd05a6042c9eb7b77f8e40
Himanshu Goel <himanshu.goel@soprabanking.com>
2021-06-13 09:13:04 +0530

    Change some namings

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/shri-gupta-cement-agency'),
+      dir: require('path').join(__dirname, './coverage/shree-gupta-cement-distributors'),
       subdir: '.',
       reporters: [
         { type: 'html' },
